2c4b445bec1c32487a0ab3be43cf6e86171d1e37
Kevin Kularathne <kevin.d.kularathna@gmail.com>
2021-12-08 10:38:51 +0530

    Update navigation_bar.php
    added script to update image

--- navigation_bar.php
+++ navigation_bar.php
@@ -5,30 +5,60 @@ include_once("jquery-theme/themes/navbar_styles.css");
 ?>
 </style>
 
+<script>
+    window.onload = function() {
+        if(window.location.pathname.endsWith("homepage.php")){
+            selectHome();
+        } else if (window.location.pathname.endsWith("my_hunts.php")) {
+            selectHunts();
+        } else if (window.location.pathname.endsWith("game.php")) {
+            selectGame();
+        } else if (window.location.pathname.endsWith("awards.php")){
+            selectAwards();
+        }
+    };
+    
+    function selectHome() {
+      document.getElementById("homeImg").src = "images/icons/homeIconSelected.png";
+    }
+
+    function selectHunts() {
+      document.getElementById("huntsImg").src = "images/icons/huntsIconSelected.png";
+    }
+
+    function selectGame() {
+      document.getElementById("gameImg").src = "images/icons/gameIconSelected.png";
+    }
+
+    function selectAwards() {
+      document.getElementById("awardsImg").src = "images/icons/awardsIconSelected.png";
+    }
+</script>
+
 <div id="navbar" data-role="footer" data-tap-toggle="false">
     <div data-role='navbar' data-tap-toggle="false" class="nav-bar" style="overflow: hidden;">
         <ul>
             <li>
-                <a href="homepage.php" data-ajax="false" id="navHome" class="nav-bar-item">
-                    <img class="navBarIcon" src="images/icons/homeIcon.png" alt="home"/>
+                <a onclick="selectHome()" href="homepage.php" data-ajax="false" id="navHome" class="nav-bar-item">
+                    <img id="homeImg" class="navBarIcon" src="images/icons/homeIcon.png" alt="home"/>
                     <h4 class="nav-bar-item-option">HOME</h4>
                 </a>
             </li>
             <li>
-                <a href="my_hunts.php" data-ajax="false" id="navHunts" rel="dialog" class="nav-bar-item">
-                    <img class="navBarIcon" src="images/icons/huntsIcon.png" alt="hunts"/>
+                <a onclick="selectHunts()" href="my_hunts.php" data-ajax="false" id="navHunts" rel="dialog" class="nav-bar-item">
+                    <img id="huntsImg" class="navBarIcon" src="images/icons/huntsIcon.png" alt="hunts"/>
                     <h4 class="nav-bar-item-option">HUNTS</h4>
                 </a>
             </li>
             <li>
-                <a href="treasure_hunt.php" data-ajax="false" id="navGame" class="nav-bar-item">
-                    <img class="navBarIcon" src="images/icons/gameIcon.png" alt="game"/>
+                <a onclick="selectGame()" href="treasure_hunt.php" data-ajax="false" id="navGame" class="nav-bar-item">
+                    <img id="gameImg" class="navBarIcon" src="images/icons/gameIcon.png" alt="game"/>
                     <h4 class="nav-bar-item-option">GAME</h4>
                 </a>
             </li>
             <li>
-                <a id="navAwards" href="awards.php" data-ajax="false" class="nav-bar-item">
-                    <img class="navBarIcon" src="images/icons/awardsIcon.png" alt="awards"/>
+                <a onclick="selectAwards()" id="navAwards" href="awards.php" data-ajax="false" class="nav-bar-item">
+                    <img id="awardsImg" class="navBarIcon" src="images/icons/awardsIcon.png" alt="awards"/>
                     <h4 class="nav-bar-item-option">AWARDS</h4>
                 </a>
             </li>
