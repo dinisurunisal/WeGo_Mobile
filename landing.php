cbb6c7f9006f1d9a38b36860432efed133140c16
Pasinduekanayake <60251151+Pasinduekanayake@users.noreply.github.com>
2021-12-10 19:22:28 +0530

    login update

--- landing.php
+++ landing.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"> </script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script>
 	$(document).ready(function(){
 		$("#next0").click(function(){
