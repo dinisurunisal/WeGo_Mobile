8eb47db2ea81b97698f59ead7b809a8de5d0b67a
Pasinduekanayake <60251151+Pasinduekanayake@users.noreply.github.com>
2021-12-10 22:26:34 +0530

    Update forgot password

--- forgot_password.php
+++ forgot_password.php
@@ -3,39 +3,123 @@
 	<head>
 		<!-- Common -->
 		<meta charset="utf-8">
-		<title>Destination</title>
+		<title>Forgot Password</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="css/themes/my-custom-theme.css" />
-		<link rel="stylesheet" href="css/themes/common_themes.css" />
-		<link rel="stylesheet" href="css/themes/jquery.mobile.icons.min.css" />
 
 		<!-- The three things that jQuery Mobile needs to work -->
 		<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
 		<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
 		<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
-		<!-- Custom -->
-		<script src="js/destination.js"></script>
-
 	</head>
+	
+	<style>
+		#forgotPassword{
+			background-image: url('images/signupBackground.png');
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+			background-size: 376px;
+		}
+		.formDiv{
+			height: 500px;
+			width: 300px;
+			margin-top: 15%;
+			margin-left: 10%;
+		}
+		.ui-page-theme-a .ui-body-inherit{
+			color: white;
+			min-height: 2.2em;
+			text-align: left;
+			border-color: white;
+			border-radius: 5px;
+			background: rgba(0, 0, 0, 0.2);
+			line-height: 1.4em;
+			box-sizing: border-box;
+			margin-top: 2px;
+			margin-bottom: 15px;
+			width: -webkit-fill-available;
+		}
+		A {text-decoration: none;} 
+		.anchorH4{
+			font-family: Noto Sans;
+			font-size: 15px; 
+			font-weight: 500;
+			color: white;
+			margin-top: 10%; 
+			margin-bottom: 20%;
+			margin-left: 5%;
+			text-align: left;
+		}
+		.wrapper {
+			margin-top:15%;
+			text-align: center;
+		}
+		#buttons{
+			margin-left: auto;
+			margin-right: auto;
+			height:41px; 
+			width:215px; 
+			font-family: Noto Sans; 
+			font-size: 15px; 
+			font-weight: 900;
+			font-color:white; 
+			border-radius:45px; 
+			color:#0174CF; 
+			background-color:white; 
+			border:none;
+		}
+		.labelform{
+			font-family: Noto Sans; 
+			font-size: 15px; 
+			font-weight: 500;
+			color:white;
+		}
+		h1{
+			text-align: center;
+			font-family: Noto Sans; 
+			font-weight: 900; 
+			font-size: 30px; 
+			line-height: 28px; 
+			letter-spacing: 0.01em; 
+			color: #ffffff;
+		}
+		p{
+			text-align: center;
+			font-family: Noto Sans; 
+			font-weight: normal; 
+			font-size: 17px; 
+			line-height: 0; 
+			letter-spacing: 0.01em; 
+			color: #ffffff;
+		}
+	</style>
 
 	<body>
-		<div data-role="page" id="destination_page">
-			<!-- Header -->
-			<?php include("header.php"); ?>
-
-			<div role="main" class="ui-content">
-
-				<div class="col-md-6 col-sm-6 col-xs-12 fill" style="float:left; background-color: #2c2e3c; padding:0px 0px;">
-					<img src="images/background_find.jpg">
-				</div>
+		<div data-role="page" id="forgotPassword">
+			<div role="main">
+				<a href="login.php" data-transition="slide" data-ajax="false" id="flat-btn">
+					<h4 class="anchorH4">Back</h4>
+                </a>
+				<h1>
+					Forgot Password
+				</h1>
+				<p>
+					You can reset your password here
+				</p>
+				<div class="formDiv">				
+                        <form id="form" onsubmit="event.preventDefault(); login()">
+                            <label class="labelform" for="contactNumber"> Conatact Number </label>
+                            <input required type="number" name="contactNumber" id="contactNumberForm" class="formField" />
+                           
+							<div class="wrapper">
+								<button type="submit" class="buttons" id="buttons" data-role="button">Reset</button>
+							</div>
+							
+                        </form>
+                   </div>
 			</div>
 
-			<?php include("footer.php"); ?>
-			<?php include("navigation_bar.php"); ?>
+
 		</div>
 	</body>
 </html>
