ce97a79b58c2858c4b92ae3daf81f5c731e29a61
Dinisuru.2018559@outlook.com <dinisurunisal@gmail.com>
2021-12-11 13:41:45 +0530

    common changes

--- js/destination.js
+++ js/destination.js
@@ -1,5 +1,28 @@
-$(function() {
-    $( "i" ).click(function() {
-      $( "i" ).toggleClass( "press", 1000 );
+var defaultDestinationDB = [];
+
+$(function initialization() {
+    $.getJSON("json-files/destinations.json", function (jsonFile) {
+        defaultDestinationDB = jsonFile;
+        console.log(defaultDestinationDB)
+
+        loadDestinationList();
     });
+});
+
+function loadDestinationList() {
+
+    for (var i = 0; i < defaultDestinationDB.length; i++) {
+        document.getElementById("card_image").src = defaultDestinationDB[i].destinationImage;
+        document.getElementById("card_name").innerHTML = defaultDestinationDB[i].destinationName;
+        document.getElementById("card_details").innerHTML = defaultDestinationDB[i].destinationCardDetails;
+        // document.getElementById("kitchen-rating-script").innerHTML = (defaultDestinationDB[i].kitchenRatingTotal / defaultDestinationDB[i].kitchenRatingUserNumber).toFixed(1);
+    
+        $("#card_script_1").clone().appendTo("#card_script_2");
+    }
+}
+
+$(function () {
+  $("i").click(function () {
+    $("i").toggleClass("press", 1000);
   });
+});
