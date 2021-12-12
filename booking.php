44e6aa076476840f39ced9de5b5a20a22a35197d
Pasinduekanayake <60251151+Pasinduekanayake@users.noreply.github.com>
2021-12-12 22:52:04 +0530

    update booking

--- booking.php
+++ booking.php
@@ -1,41 +1,41 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<!-- Common -->
-		<meta charset="utf-8">
-		<title>Destination</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="css/themes/my-custom-theme.css" />
-		<link rel="stylesheet" href="jquery-theme/themes/theme.min.css" />
-		<link rel="stylesheet" href="jquery-theme/themes/jquery.mobile.icons.min.css" />
+<head>
+	<!-- Common -->
+	<meta charset="utf-8">
+	<title>Destination</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<!-- The three things that jQuery Mobile needs to work -->
-		<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
-		<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-		<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+	<!-- The three things that jQuery Mobile needs to work -->
+	<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+	<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
-		<!-- Custom -->
-		<script src="js/destination.js"></script>
+	<!--bootstrap-->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-	</head>
+	<!-- Custom -->
+	<link rel="stylesheet" href="css/style.css" />
 
-	<body>
-		<div data-role="page" id="destination_page">
-			<!-- Header -->
-			<?php include("header.php"); ?>
+</head>
 
-			<div role="main" class="ui-content">
+<body>
+	<div data-role="page" id="booking_page" class="booking_page">
+		<!-- Header -->
+		<?php include("header.php"); ?>
 
-				<div class="col-md-6 col-sm-6 col-xs-12 fill" style="float:left; background-color: #2c2e3c; padding:0px 0px;">
-					<img src="images/background_find.jpg">
-				</div>
+		<div role="main" class="row">
+
+			<div class="col-md-12" style="float:left; background-color: #2c2e3c; padding:0px 0px;">
+
 			</div>
-
-			<?php include("footer.php"); ?>
-			<?php include("navigation_bar.php"); ?>
 		</div>
-	</body>
+
+		<?php include("footer.php"); ?>
+		<?php include("navigation_bar.php"); ?>
+	</div>
+</body>
+
 </html>
