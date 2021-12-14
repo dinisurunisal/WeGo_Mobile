89c8c7937c2da9a8f7eed528e067cd60f5ed9c0c
Dinisuru.2018559@outlook.com <dinisurunisal@gmail.com>
2021-12-15 02:52:39 +0530

    selected destination screen in progress

--- destination_selected.php
+++ destination_selected.php
@@ -3,14 +3,22 @@
 	<head>
 		<!-- Common -->
 		<meta charset="utf-8">
-		<title>Destination</title>
+		<title>Destination Selected</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="css/themes/my-custom-theme.css" />
+		<!-- <link rel="stylesheet" href="css/themes/my-custom-theme.css" /> -->
+		<link rel="stylesheet" href="css/destination_selected.css" /> 
 		<link rel="stylesheet" href="jquery-theme/themes/theme.min.css" />
 		<link rel="stylesheet" href="jquery-theme/themes/jquery.mobile.icons.min.css" />
+
+		<!-- font awesome link -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<!-- bootstrap -->
+		<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
 
 		<!-- The three things that jQuery Mobile needs to work -->
 		<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
@@ -18,21 +26,35 @@
 		<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
 		<!-- Custom -->
-		<script src="js/destination.js"></script>
+		<script src="js/destination_selected.js"></script>
 
 	</head>
 
 	<body>
-		<div data-role="page" id="destination_page" data-theme="d">
+
+		<div data-role="page" id="destination_page" class="destination_page">
 			<!-- Header -->
 			<?php include("header.php"); ?>
 
-			<div role="main" class="ui-content">
+			<div class="dest_background_image" style="margin-top: 50px;">
+				<div class="dest_main_intro">
+				</div>
+				<img src="images/background_find.jpg" id="london_bg" alt="london_bg" width="100%">
 
-				<div class="col-md-6 col-sm-6 col-xs-12 fill" style="float:left; background-color: #2c2e3c; padding:0px 0px;">
-					<img src="images/background_find.jpg">
+				<div class="dest_content_block">
+					<div style="position: relative;">
+						dddd
+					</div>
+					<div style="position: relative;">
+						dddd
+					</div>
 				</div>
 			</div>
+
+			<div role="main" class="ui-content" style="height: 450px;">
+		
+			</div>
+
 
 			<?php include("footer.php"); ?>
 			<?php include("navigation_bar.php"); ?>
---
+++ js/destination_list.js
@@ -0,0 +1,109 @@
+const dollarSign = "$ "
+var defaultDestinationDB = [];
+
+$(function initialization() {
+    $.getJSON("json-files/destinations.json", function (jsonFile) {
+        defaultDestinationDB = jsonFile;
+        console.log(defaultDestinationDB)
+
+        loadDestinationList();
+    });
+});
+
+function loadDestinationList() {
+
+    for (var i = 0; i < defaultDestinationDB.length; i++) {
+        document.getElementById("card_image").src = defaultDestinationDB[i].destinationImage;
+        document.getElementById("card_name").innerHTML = defaultDestinationDB[i].destinationName;
+        document.getElementById("card_details").innerHTML = defaultDestinationDB[i].destinationCardDetails;
+        document.getElementById("card_price").innerHTML = dollarSign + defaultDestinationDB[i].price;
+        // document.getElementById("kitchen-rating-script").innerHTML = (defaultDestinationDB[i].kitchenRatingTotal / defaultDestinationDB[i].kitchenRatingUserNumber).toFixed(1);
+    
+        $("#card_script_1").clone().appendTo("#card_script_2");
+    }
+
+    deleteDuplicateCards(); 
+}
+
+
+function popupDestinationRestoreDefault() {
+  restoreDefaultFilter(); // This function can be found in "destination_filter_popup.js" file
+}
+
+// Delete 'card_script_1'
+function deleteDuplicateCards() {
+  var id = document.getElementById("card_script_1");
+  id.parentNode.removeChild(id);
+}
+
+function onDestinationSelect(id) {
+  if (typeof (Storage) !== "undefined") {
+    localStorage.setItem("clickedDestinationId", clickedId);
+    window.location = "destination_selected.php";
+  } else {
+      showFailure("Unable to load")
+  }
+}
+
+$(function () {
+  $("i").click(function () {
+    $("i").toggleClass("press", 1000);
+  });
+});
+
+function addToFavourites(id) {
+
+  if (!e) var e = window.event;
+  e.cancelBubble = true;
+  if (e.stopPropagation)
+      e.stopPropagation();
+
+  var favVal = $("#" + id).html();
+  var users = JSON.parse(localStorage.getItem('users'));
+  var sellers = JSON.parse(localStorage.getItem('sellers'));
+
+  var currentlySignedInUser = JSON.parse(localStorage.getItem('currentlySignedInUser'));
+  if (favVal == "favorite") {
+      $.each(sellers, function (key, seller) {
+          if (seller.sellerId == id.replace("favourite", "")) {
+              $.each(users, function (key, user) {
+                  if (user.contactNumber == currentlySignedInUser.contactNumber) {
+
+                      var filteredUser = user.favouritesList.filter(function (value, index, arr) {
+                          return value.sellerId != seller.sellerId
+
+                      });
+                      user.favouritesList = filteredUser;
+                      var filteredCurrentUser = currentlySignedInUser.favouritesList.filter(function (value, index, arr) {
+                          return value.sellerId != seller.sellerId
+                      });
+                      currentlySignedInUser.favouritesList = filteredCurrentUser;
+                      localStorage.setItem("users", JSON.stringify(users));
+                      localStorage.setItem("currentlySignedInUser", JSON.stringify(currentlySignedInUser));
+                      $("#" + id).html("favorite_border");
+                  }
+              });
+          }
+      });
+  } else {
+      console.log(sellers);
+      $.each(sellers, function (key, seller) {
+          if (seller.sellerId == id.replace("favourite", "")) {
+              $.each(users, function (key, user) {
+                  if (user.contactNumber == currentlySignedInUser.contactNumber) {
+                      user.favouritesList.push({ 'sellerId': seller.sellerId, 'kitchenName': seller.kitchenName });
+                      currentlySignedInUser.favouritesList.push({ 'sellerId': seller.sellerId, 'kitchenName': seller.kitchenName });
+                      localStorage.setItem("users", JSON.stringify(users));
+                      localStorage.setItem("currentlySignedInUser", JSON.stringify(currentlySignedInUser));
+                      $("#" + id).html("favorite");
+                      showSuccess("Added to Favourites List")
+
+                  }
+              });
+          }
+      });
+
+
+  }
+
+}
