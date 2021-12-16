0e89c2b414c1b38b20a623d72c25a7786143820a
Kevin Kularathne <kevin.d.kularathna@gmail.com>
2021-12-17 03:23:06 +0530

    added my hunt past card

--- my_hunts.php
+++ my_hunts.php
@@ -17,6 +17,57 @@
     <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
     <script type="text/javascript" src="js/my_hunts.js"></script>
     
+    <style>
+        .past_list {
+            padding: 8px 0px 8px 0px!important;
+            display: grid;
+            grid-template: auto auto;
+        }
+        
+        .past_card {
+            background-color: white;
+            border-radius: 12px;
+            height: 265px;
+            width: fit-content;
+            margin: 8px 8px 8px 8px;
+        }
+        
+        .card_image_section {
+            border-top-left-radius: 12px;
+            border-bottom-left-radius: 12px;
+            width: 35%;
+            display: grid;
+            float: left;
+        }
+        
+        .card_image_section img{
+            border-top-left-radius: 12px;
+            border-bottom-left-radius: 12px;
+            width: 100%;
+            height: 265px;
+            object-position: -180px;
+            object-fit: cover;
+        }
+        
+        .past_card_content {
+            display: grid;
+            padding: 8px;
+            text-align: left;
+        }
+        
+        .past_card_content h3, p2 {
+            width: fit-content;
+            margin-top: 4px;
+            margin-bottom: 4px;
+        }
+        .feedback_btn {
+            width: 158px !important;
+            background-color: #0174cf !important;
+            color: white !important;
+            text-shadow: none !important;
+        }
+    </style>
+    
 </head>
 
 <body>
@@ -55,6 +106,38 @@
                 <h4 style="text-align: center;">Book locations to add them to your upcoming hunts.</h4>
             </div>
             <div id="past-empty">
+                <div class="past_list">
+                    
+                    <div class="past_card">
+                        <div class="card_image_section">
+                            <img src="images/destinations/Big Ben.jpg" class="card_image">
+                        </div>
+                        <div class="past_card_content">
+                            <h3 class="past_location_name">Big Ben</h3>
+                            <p2 class="past_tour_date">Tour completed on 25-12-2021</p2>
+                            <hr>
+                            <h3 class="past_location_route">EUS - DEL</h3>
+                            <h3 class="past_location_duration">Duration: 1h 11m</h3>
+                            <h3 class="past_location_trip">One Way</h3>
+                            <button class="feedback_btn">Leave feedback</button>
+                        </div>
+                    </div>
+
+                    <div class="past_card">
+                        <div class="card_image_section">
+                            <img src="images/destinations/The%20British%20Museum.jpg" class="card_image">
+                        </div>
+                        <div class="past_card_content">
+                            <h3 class="past_location_name">Big Ben</h3>
+                            <p2 class="past_tour_date">Tour completed on 25-12-2021</p2>
+                            <hr>
+                            <h3 class="past_location_route">EUS - DEL</h3>
+                            <h3 class="past_location_duration">Duration: 1h 11m</h3>
+                            <h3 class="past_location_trip">One Way</h3>
+                            <button class="feedback_btn">Leave feedback</button>
+                        </div>
+                    </div>
+                </div>
                 <h4 style="text-align: center; ">No locations hunted yet.</h4>
             </div>
         </div>
