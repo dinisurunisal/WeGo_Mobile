836d906af4134dfb7af19b365a203da0978bae8c
Kevin Kularathne <kevin.d.kularathna@gmail.com>
2021-12-23 13:35:22 +0530

    added profile page hyperlink

--- header.php
+++ header.php
@@ -14,6 +14,6 @@
         <img id="logo_image" src="images/HeaderLogo.png" alt="WeGo logo"/>
     </div>
     <div id="common_header_profile_icon">
-        <img id="profile_image" src="images/user.png" alt="profile"/>
+        <a href="profile.php"><img id="profile_image" src="images/user.png" alt="profile"/> </a>
     </div>
 </div>
