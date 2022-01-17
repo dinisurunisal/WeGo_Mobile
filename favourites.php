8e94ea02d60588f25be4609428264e6140673ea8
Pasinduekanayake <60251151+Pasinduekanayake@users.noreply.github.com>
2022-01-18 02:19:44 +0530

    updated

--- favourites.php
+++ favourites.php
@@ -18,6 +18,7 @@
 	<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
 	<!-- Custom -->
+	
 	<link rel="stylesheet" href="css/style.css" />
 	<script src="js/favorites_list_share.js"></script>
 
