ae09907812ef4b191bf842bfd6ca2f6b7a0e1950
Dinisuru.2018559@outlook.com <dinisurunisal@gmail.com>
2022-01-18 00:04:24 +0530

    treasure hunt search fixed

--- js/treasure_hunt.js
+++ js/treasure_hunt.js
@@ -69,20 +69,3 @@ function addMarkerWithTimeout(position, timeout, title, content) {
     
   }, timeout);
 }
- 
-function onGameSearchClick() {
-  var userInputValueInHeaderSearch = document.getElementById("game-search-box").value;
-
-  if (userInputValueInHeaderSearch == "") {
-      showFailure("Enter a destination name!");
-  } else {
-      localStorage.setItem("filterDB", null);
-      if (typeof (Storage) !== "undefined") {
-          localStorage.setItem("userSearchValue", JSON.stringify({ "type": "search", "value": userInputValueInHeaderSearch }));
-          console.log(localStorage)
-          // window.location = "browse_kitchen.php";
-      } else {
-          showFailure("Your browser does not support Frese");
-      }
-  }
-}
