587ff11194cef745e699d175569ace1740d7e59d
Kevin Kularathne <kevin.d.kularathna@gmail.com>
2022-01-18 17:53:14 +0530

    code clean

--- my_hunts_feedback.php
+++ my_hunts_feedback.php
@@ -2,14 +2,11 @@
     <?php include_once("css/style.css"); include_once("jquery-theme/themes/theme.css");?>
 </style>
 
-<!-- <script src="js/footer_script.js"></script> -->
-
 <div data-role="popup" data-transition="pop" style="padding: 20px;" id="my_hunts_feedback" class="ui-content">
     <form id="form" onsubmit="event.preventDefault(); submitFeedback()">
         <h2>How was your experience?</h2>
         <p2>Let us know what you think</p2>
-<!--                    <label class="labelform grey heading-five" for="password"> Rating </label>-->
-    
+
 		<div id="star_rating_holder">
 			<i id="star_rating_one" class="material-icons" style="font-size: 40px; color:red">star_border</i>
 			<i id="star_rating_two" class="material-icons" style="font-size: 40px; color:red">star_border</i>
@@ -18,7 +15,6 @@
 			<i id="star_rating_five" class="material-icons" style="font-size: 40px; color:red">star_border</i>
 		</div>
 
-		<!-- <img src="images/fourStars.png" style="width: 70%; padding: 8px 0 8px 0;"> -->
 		<textarea type="text" required name="comment" id="form_comment" class="formField" placeholder="Enter your comment here"></textarea>
 		<div class="form_button_container">
 			<a data-rel="back"><button class="cancel_btn">Cancel</button></a>
